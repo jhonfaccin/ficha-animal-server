9f1b487cc70bfcfe39ba3c1ff4605eea9877543f
jhon faccin <jhonfaccin@gmail.com>
2018-09-11 14:28:34 -0300

    iniciando o projeto

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+mvn clean package
+
+wildfly=/home/jhon/dev/sourse/wildfly-10.1.0.Final
+
+cp target/ficha-animal-server.war $wildfly/standalone/deployments/
---
+++ src/main/java/br/com/jhonfaccin/api/PingResource.java
@@ -0,0 +1,14 @@
+package br.com.jhonfaccin.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("ping")
+public class PingResource {
+
+    @GET
+    public String ping() {
+        return "Enjoy Java EE 8!";
+    }
+
+}
